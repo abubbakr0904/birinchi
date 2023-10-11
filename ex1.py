dc5d9aac780f66e18508dcc35bb7bbaa7cd409fe
abubbakr0904 <abubbakroqilov03@gmail.com>
2023-10-11 19:06:15 +0300

    kkkkkkk

--- ex1.py
+++ ex1.py
@@ -7,7 +7,6 @@ oyy = sana[1]
 for i in  oy.keys(): 
     g = " "
     if "0" in sana[0]:
-        print(g)
         g += sana[0][-1]
     else:
         g += sana[0]
---
+++ ex2.py
@@ -0,0 +1,1 @@
+print("Assalom dunyo ")
